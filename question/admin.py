c2289f03c6e1881e4110ec355d219d6d97ae60d2
Andrii Rykhliuk <andr.ruhx@gmail.com>
2025-09-20 20:04:49 +0200

    Add custom admin fields to admin panel

--- question/admin.py
+++ question/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from question.models import Question
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "difficulty", "is_active", "topic")
+    search_fields = ("question_text",)
+    list_filter = ("difficulty", "topic__name", "source", "is_active")
+    readonly_fields = ("created_at", "updated_at")
